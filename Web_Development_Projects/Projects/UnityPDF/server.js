35803119cc983af961523b80a4d701bc8cbee4ae
Shriya S Bhandare <shriyasbhandare2006@gmail.com>
2024-05-03 12:21:20 +0530

    Recovering my node project of unitypdf!

--- Web_Development_Projects/Projects/UnityPDF/server.js
+++ Web_Development_Projects/Projects/UnityPDF/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require("path")
 const multer = require('multer')
-const mergedPdfs = require('./merge')
+const { mergedPdfs } = require('./merge')
 
 const upload = multer({ dest: 'uploads/' })
 const app = express()
@@ -12,7 +12,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "templates/index.html"))
 })
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
-    console.log(req.files)
     let d = await mergedPdfs(path.join(__dirname, req.files[0].path), (path.join(__dirname, req.files[1].path)))
     res.redirect(`http://localhost:4000/static/${d}.pdf`)
 })
